627d577499b2e8234d3249f76083d8c6918a6d44
hapooisluv <yeonjun.thai@gmail.com>
2025-11-28 21:58:37 +0700

    Added tty and vga support for the kernel now we can print things out to the tty All written in rust with safe abstraction and the 'rusty' way ;)

--- src/arch/x86_64/tty/mod.rs
+++ src/arch/x86_64/tty/mod.rs
@@ -1,21 +1,19 @@
 use crate::arch::x86_64::vga::{VgaColor, vga_entry, vga_entry_color};
-
 const TTY_WIDTH: usize = 80;
 const TTY_HEIGHT: usize = 25;
-const VGA_MEM: *mut u16 = 0xB800 as *mut u16;
+const VGA_MEM: *mut u16 = 0xB8000 as *mut u16;
 
-struct Terminal {
+pub struct Terminal {
     terminal_row: usize,    // Height
     terminal_column: usize, // Width
     terminal_color: u8,
-    terminal_buf: *mut u16, // Bare in mind that this is not a reference but a ptr
 }
 
 impl Terminal {
     /// initializes the terminal.
     /// As a part of initialization, it sets all the VGA entrys into terminal color.
     /// This is function does include unsafe but the function gives you a safe abstraction
-    fn new() -> Self {
+    pub fn new() -> Self {
         let terminal_color = vga_entry_color(VgaColor::WHITE, VgaColor::BLACK);
         for y in 0..TTY_HEIGHT {
             for x in 0..TTY_WIDTH {
@@ -29,31 +27,39 @@ impl Terminal {
             terminal_row: 0,
             terminal_column: 0,
             terminal_color,
-            terminal_buf: VGA_MEM,
         }
     }
 
-    fn put_entry_at(character: char, color: u8, x: usize, y: usize) {
+    fn _put_entry_at(character: char, color: u8, x: usize, y: usize) {
         let index: usize = y * TTY_WIDTH + x;
         unsafe { *VGA_MEM.add(index) = vga_entry(character, color) }
     }
 
-    fn put_char(&mut self, char: char) {
-        Terminal::put_entry_at(
+    fn _put_char(&mut self, char: char) {
+        Terminal::_put_entry_at(
             char,
             self.terminal_color,
             self.terminal_column,
             self.terminal_row,
         );
-        if self.terminal_column == TTY_WIDTH {
-            self.terminal_column = 0;
-            if self.terminal_row == TTY_HEIGHT {
-                self.terminal_row = 0;
-            } else {
-                self.terminal_row += 1;
+
+        match self.terminal_column + 1 {
+            column if column == TTY_WIDTH => {
+                self.terminal_column = 0;
+                match self.terminal_row + 1 {
+                    row if row == TTY_HEIGHT => self.terminal_row = 0,
+                    row => self.terminal_row = row,
+                }
             }
-        } else {
-            self.terminal_column += 1;
+            column => self.terminal_column = column,
+        }
+    }
+
+    /// For future reference, this function is exposed as a public method just for now,
+    /// one must prefer the println! macro over at the future when the macro is implemented.
+    pub fn terminal_write(&mut self, string: &str) {
+        for char in string.chars() {
+            self._put_char(char);
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,17 @@
 #![no_main]
 
 mod arch {
-    mod x86_64;
+    pub mod x86_64;
 }
 
+use crate::arch::x86_64::tty::Terminal;
 use core::arch::asm;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn kmain() -> ! {
+    let mut terminal = Terminal::new();
+    terminal.terminal_write("Hello kernel!");
+
     loop {
         unsafe {
             asm!("hlt");
